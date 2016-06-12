95c568723527877bbb5fc8948ae7d7f42896220a
Daniel Malik <Daniel Malik>
2016-06-12 02:30:46 +0200

    Frontend for the web

--- Frontend/index.php
+++ Frontend/index.php
@@ -44,16 +44,15 @@
 					</button>
 					<h4 class="modal-title" id="myModalLabel">Settings</h4>
 				</div>
-				<form class="form-control input-group" method="POST"
-					action="submit.php">
-					<div class="modal-body">
+				<form class="form-control input-group" id="settingsform">
+					<div class="modal-body" id="settingsbody">
 						<label for="recipient-name" class="control-label">Account settings</label>
 						<div class="input-group">
 							<!-- Email start -->
 							<span class="input-group-addon" id="basic-addon3">Email:</span> <input
-								type="email" class="form-control" name="email" autocomplete="off"
-								aria-describedby="basic-addon3"
-								placeholder="<?php
+								id="email" type="email" class="form-control" name="email"
+								autocomplete="off" aria-describedby="basic-addon3"
+								value="<?php
 								$conn = mysqli_connect ( '192.168.2.102', 'data', '123456', 'comHome' );
 								$result = mysqli_query ( $conn, "SELECT `Value` FROM `Config` WHERE `Key`='Email'" );
 								$res = mysqli_fetch_array ( $result );
@@ -63,20 +62,53 @@
 						<div class="input-group">
 							<!-- IP Start -->
 							<span class="input-group-addon" id="basic-addon3">Your static IP
-								Address:</span> <input type="text" class="form-control"
+								Address:</span> <input type="text" class="form-control" id="ip"
 								name="ip" aria-describedby="basic-addon3" autocomplete="off"
-								placeholder="<?php
+								value="<?php
 								$conn = mysqli_connect ( '192.168.2.102', 'data', '123456', 'comHome' );
 								$result = mysqli_query ( $conn, "SELECT `Value` FROM `Config` WHERE `Key`='userIP'" );
 								$res = mysqli_fetch_array ( $result );
 								echo $res [0];
 								?>" />
 						</div>
+						<br />
+						<!-- Notification -->
+						<label for="recipient-name" class="control-label">Sensor settings</label>
+						<!-- Sensor settings -->
+						<div class="input-group">
+							<span class="input-group-addon" id="basic-addon3">Name for sensor
+								1:</span> <input id="sensor1" type="text" class="form-control"
+								name="sensor1" autocomplete="off"
+								aria-describedby="basic-addon3"
+								value="<?php
+								$conn = mysqli_connect ( '192.168.2.102', 'data', '123456', 'comHome' );
+								$result = mysqli_query ( $conn, "SELECT `Name` FROM `Sensors` WHERE `ID`='1'" );
+								$res = mysqli_fetch_array ( $result );
+								echo $res [0];
+								?>" />
+						</div>
+						<div class="input-group">
+							<span class="input-group-addon" id="basic-addon3">Name for sensor
+								2:</span> <input type="text" class="form-control" id="sensor2"
+								name="sensor2" aria-describedby="basic-addon3"
+								autocomplete="off"
+								value="<?php
+								$conn = mysqli_connect ( '192.168.2.102', 'data', '123456', 'comHome' );
+								$result = mysqli_query ( $conn, "SELECT `Name` FROM `Sensors` WHERE `ID`='2'" );
+								$res = mysqli_fetch_array ( $result );
+								echo $res [0];
+								?>" />
+						</div>
+						<div id="settingsnotification"></div>
+						<br />
+
 					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="submit" class="btn btn-primary">Save changes</button>
+						<button type="button" id="apply" class="btn btn-primary">Save
+							changes</button>
 					</div>
+
 				</form>
 			</div>
 		</div>
@@ -90,13 +122,55 @@
 					data-toggle="table">
 
 				</table>
+				<br />
+				<h2>Statistics:</h2>
+				<script src="js/chart.js"></script>
+<canvas id="myChart" width="400" height="400"></canvas>
+<script>
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>
 
 			</div>
 		</div>
 	</div>
 	<script src="js/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script>
+	<script src="js/bootstrap.min.js"></script><script>
 						$(document).ready(function(){
 							window.setInterval(function(){
 								$.ajax("http://localhost/Eclipse/Projects/comHome/update.php", {
@@ -110,7 +184,33 @@
 							});
 
 	</script>
+	<script type="text/javascript">
+							$("#apply").click(function() {
+									//BTNCLCK
+								 var values = $("#settingsform").serialize();
+								 $.ajax({
+								        url: "submit.php",
+								        type: "post",
+								        data: values ,
+								        success: function (response) {
+								          if(response == "emailips1s2"){
+								        	$("#settingsnotification").html('<div class="alert alert-success" role="alert">Your settings were updated successfully.</div>');
+								        	}else {
+									        	$("#settingsnotification").html('<div class="alert alert-warning" role="alert">Don\'t leave any fields empty!</div>');
+													
+									        	}
+									        
+								        },
+								        error: function(jqXHR, textStatus, errorThrown) {
+								        	$("#settingsnotification").html('<div class="alert alert-danger" role="alert">An error occoured while updating your settings!' + textStatus +'</div>');
+								        }
 
+
+								    });
+
+								
+								});
+	</script>
 
 
 </body>
--- Frontend/submit.php
+++ Frontend/submit.php
@@ -1,9 +1,36 @@
 <?php
 //Submit to shit
 $email = $_POST['email'];
+$ip = $_POST['ip'];
+$s1 = $_POST['sensor1'];
+$s2 = $_POST['sensor2'];
 $conn = mysqli_connect ( '192.168.2.102', 'data', '123456', 'comHome' );
+//MAIL
 if(!empty($email)){
 $result = mysqli_query($conn, "UPDATE `Config` SET `Value`='" . $email . "' WHERE `Key`='Email'");
-mysqli_close($conn);
+echo "email";
+}else {
+	echo "emailerror";
+}
+//IP
+if(!empty($ip)){
+	$result = mysqli_query($conn, "UPDATE `Config` SET `Value`='" . $ip . "' WHERE `Key`='userIP'");
+	echo "ip";
+}else {
+	echo "iperror";
+}
+//s1
+if(!empty($s1)){
+	$result = mysqli_query($conn, "UPDATE `Sensors` SET `Name`='" . $s1 . "' WHERE `ID`='1'");
+	echo "s1";
+}else {
+	echo "s1error";
+}
+//s2
+if(!empty($s2)){
+	$result = mysqli_query($conn, "UPDATE `Sensors` SET `Name`='" . $s2 . "' WHERE `ID`='2'");
+	echo "s2";
+}else {
+	echo "s2error";
 }
 ?>
